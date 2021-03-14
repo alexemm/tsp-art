0f4d19bc22e94be5139c53212bfab6b6ce56f8e2
Alexander Emmerich <al_emmerich@gmx.de>
2021-03-14 15:20:01 +0100

    Fixed mistakes + added a conversion from image to tsp art

--- image_tools.py
+++ image_tools.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image, ImageDraw
 from concorde.tsp import TSPSolver
+from dotter.stippler import tractor_beam
 
 
 def load_image(file: str):
@@ -16,8 +17,8 @@ def array_to_image(arr):
     return Image.fromarray(np.uint8(arr))
 
 
-def image_to_tsp_nodes(img):
-    arr = image_to_array(img)
+def array_to_tsp_nodes(arr):
+    #arr = image_to_array(img)
     points = np.argwhere(arr == 0)
     return points
 
@@ -28,9 +29,10 @@ def get_header(f, name, dim):
     return lines
 
 
-def parse_to_tsp_file(input_folder, name):
+def parse_to_tsp_file(input_folder, name, nodes):
+    print(nodes)
     img = load_image(input_folder + name)
-    nodes = image_to_tsp_nodes(img)
+    #nodes = image_to_tsp_nodes(img)
     with open(input_folder + name.split('.')[0] + ".tsp", 'w') as f:
         lines = get_header(f, name, len(nodes))
         lines += ["%i %i %i\n" % (i, point[0], point[1]) for i, point in enumerate(nodes)]
@@ -50,29 +52,22 @@ def image_to_tsp_routed():
     input_folder = 'input/'
     output_folder = 'output/'
     name = 'beeg.jpg'
-    img, nodes = parse_to_tsp_file(input_folder, name)
+    k = 1024
+    iterations = 10000
+    im_arr = image_to_array(load_image(input_folder + name))
+    # output_img = array_to_image(im_arr)
+    output_img = tractor_beam(im_arr, k=k, iterations=iterations)
+    #output_img.save(output_folder + name)
+    img, nodes = parse_to_tsp_file(input_folder, name, array_to_tsp_nodes(output_img))
     solution = solve_tsp(input_folder, output_folder, name.split('.')[0] + '.tsp').tour
     solution += solution[0]
-    draw = ImageDraw.Draw(img)
+    im = Image.new('RGB', img.size, (255, 255, 255, 0))
+    draw = ImageDraw.Draw(im)
     for i, node in enumerate(solution[:-1]):
         node1 = nodes[node]
         node2 = nodes[solution[i+1]]
         draw.line((node1[0], node1[1], node2[0], node2[1]), fill=128)
-    img.save(output_folder + name.split('.')[0] + '.jpg')
-
-
-
-#def draw_tsp_route()
+    im.save(output_folder + name.split('.')[0] + '.jpg')
 
 
 image_to_tsp_routed()
-
-# input_folder = 'images/input/'
-# output_folder = 'images/output/'
-# name = 'adam2battleroyale.png'
-# k = 3072
-# iterations = 10000
-# im_arr = image_to_array(load_image(input_folder + name))
-# output_img = array_to_image(im_arr)
-# output_img = array_to_image(tractor_beam(im_arr, k=k, iterations=iterations))
-# output_img.save(output_folder+name)
