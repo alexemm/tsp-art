7858523f03426bdafb6ab121d154690d4b3ef93c
Alexander Emmerich <al_emmerich@gmx.de>
2021-03-23 19:58:02 +0100

    Added functions to create art from non-optimal solutions

--- image_tools.py
+++ image_tools.py
@@ -1,7 +1,10 @@
+from typing import List
+
 import numpy as np
 from PIL import Image, ImageDraw
 from concorde.tsp import TSPSolver
 from dotter.stippler import tractor_beam
+from os import listdir
 
 
 def load_image(file: str):
@@ -40,33 +43,51 @@ def parse_to_tsp_file(input_folder, name, nodes):
     return img, nodes
 
 
-def solve_tsp(input_folder, output_folder, name):
+def solve_tsp(input_folder, name):
     solver = TSPSolver.from_tspfile(input_folder + name)
     solution = solver.solve()
-    with open(output_folder + name.split('.')[0] +'.txt', 'w') as f:
-        f.write(str(solution.tour))
+    #with open(output_folder + name.split('.')[0] +'.txt', 'w') as f:
+    #    f.write(str(solution.tour))
     return solution
+
+
+def read_solution_file(file_name: str) -> List[int]:
+    pass
+
+
+def get_solution_files_of_tsp(directory) -> List[str]:
+    return [file for file in listdir(directory) if file.split(".")[-1] == '.sol']
+
+
+def create_tsp_art(nodes, solution_tour, size) -> Image:
+    im = Image.new('RGB', size, (255, 255, 255, 0))
+    draw = ImageDraw.Draw(im)
+    for i, node in enumerate(solution_tour[:-1]):
+        node1 = nodes[node]
+        node2 = nodes[solution_tour[i + 1]]
+        draw.line((node1[1], node1[0], node2[1], node2[0]), fill=128)
+    return im
+
+
+def create_tsp_art_from_partial_solutions() -> None:
+    for i, sol in enumerate(get_solution_files_of_tsp('')):
+        pass
 
 
 def image_to_tsp_routed():
     input_folder = 'input/'
     output_folder = 'output/'
-    name = 'rocket.jpg'
-    k = 1024
+    name = 'jesus_close.jpg'
+    k = 4096
     iterations = 10000
     im_arr = image_to_array(load_image(input_folder + name))
     # output_img = array_to_image(im_arr)
     output_img = tractor_beam(im_arr, k=k, iterations=iterations)
     array_to_image(output_img).save(output_folder + "dotted_" + name)
     img, nodes = parse_to_tsp_file(input_folder, name, array_to_tsp_nodes(output_img))
-    solution = solve_tsp(input_folder, output_folder, name.split('.')[0] + '.tsp').tour.tolist()
+    solution = solve_tsp(input_folder, name.split('.')[0] + '.tsp').tour.tolist()
     solution.append(solution[0])
-    im = Image.new('RGB', img.size, (255, 255, 255, 0))
-    draw = ImageDraw.Draw(im)
-    for i, node in enumerate(solution[:-1]):
-        node1 = nodes[node]
-        node2 = nodes[solution[i+1]]
-        draw.line((node1[1], node1[0], node2[1], node2[0]), fill=128)
+    im = create_tsp_art(img.size)
     im.save(output_folder + name.split('.')[0] + '.jpg')
 
 
