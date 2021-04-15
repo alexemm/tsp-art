ce342983c7ce7462fa9bf6df790bafd23c4b354f
Alexander Emmerich <al_emmerich@gmx.de>
2021-04-15 13:58:33 +0200

    Added gif creation in stippling + solo script

--- stippler.py
+++ stippler.py
@@ -1,7 +1,13 @@
-from random import choices, seed
+from collections import OrderedDict
+from random import choices
+from typing import Optional
 
 import numpy as np
 import argparse
+
+from PIL import ImageDraw, ImageFont
+
+from image_tools import image_to_array, load_image, array_to_image
 
 
 def get_probability_matrix(arr):
@@ -26,15 +32,15 @@ def choose_next_point(prob_arr_2d, prev_shape):
     return chosen_data_points
 
 
-def create_dotted_array(chosen_points, arr):
-    dotted_arr = np.zeros(arr.shape) + 255
+def create_dotted_array(chosen_points, shape):
+    dotted_arr = np.zeros(shape) + 255
     for point in chosen_points:
         dotted_arr[point[1], point[0]] = 0
     return dotted_arr
 
 
 def find_closest_point(p, w):
-    distance_to_w = lambda x_1, y_1: np.sqrt((x_1 - w[0])**2 + (y_1 - w[1])**2)
+    distance_to_w = lambda x_1, y_1: np.sqrt((x_1 - w[0]) ** 2 + (y_1 - w[1]) ** 2)
     min_distance = distance_to_w(p[0][0], p[0][1])
     closest_point = 0
     for i, point in enumerate(p):
@@ -47,36 +53,89 @@ def find_closest_point(p, w):
     return closest_point
 
 
-def tractor_beam(arr, k, iterations=10):
+def tractor_beam(arr, k, iterations: Optional[int] = None, intermediate_steps: Optional[int] = None):
+    iterations = iterations or 10000
+    intermediate_result_steps = intermediate_steps or (iterations + 1)
     prob_matr = get_probability_matrix(arr)
     prev_shape = prob_matr.shape
     prob_matr = prob_matr.flatten()
     p, values = choose_k_points(prob_matr, k, prev_shape)
     n = np.zeros(len(p))
+    intermediates = OrderedDict() if intermediate_steps is not None else None
     for iteration in range(iterations):
         # 1. Select tractor beam point
-        w = choose_next_point(prob_matr, prev_shape)[0]
+        #w = choose_next_point(prob_matr, prev_shape)[0]
+        w = choose_k_points(prob_matr, 1, prev_shape)[0][0]
         # 2. Find closest point to tractor beam point
         i = find_closest_point(p, w)
         # 3.
         n[i] += 1
-        p[i] = int(round(1./(n[i] + 1.) * w[0] + n[i]/float(n[i] + 1.) * p[i][0])), \
-               int(round(1./(n[i] + 1.) * w[1] + n[i]/float(n[i] + 1.) * p[i][1]))
+        #print(p[i][0] - int(round(1. / (n[i] + 1.) * w[0] + n[i] / float(n[i] + 1.) * p[i][0])) == 0)
+        p[i] = int(round(1. / (n[i] + 1.) * w[0] + n[i] / float(n[i] + 1.) * p[i][0])), \
+               int(round(1. / (n[i] + 1.) * w[1] + n[i] / float(n[i] + 1.) * p[i][1]))
 
-        if iteration % 100 == 0 or iterations/2 == iteration:
+        if iteration % 100 == 0 or iterations / 2 == iteration:
             print(iteration)
-    return create_dotted_array(p, arr)
+        if iteration % intermediate_result_steps == 0 and intermediate_steps is not None:
+            intermediates[iteration] = p.copy()
+    intermediates[iterations] = p
+    return p, intermediates
 
 
-def main():
+def write_tsp_file(output_file, nodes):
+    with open(output_file, 'w') as f:
+        name: str = output_file.split('/')[-1].split('.')[0]
+        lines = get_header(name, len(nodes))
+        lines += ["%i %i %i\n" % (i, point[0], point[1]) for i, point in enumerate(nodes)]
+        f.writelines(lines)
+
+
+def get_header(name, dim):
+    lines = ["NAME: %s\n" % name, "TYPE: TSP\n", "DIMENSION: %i\n" % dim, "EDGE_WEIGHT_TYPE: EUC_2D\n",
+             "NODE_COORD_SECTION\n"]
+    return lines
+
+
+def define_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", help="File input")
     parser.add_argument("k", help="Number of dots in image", type=int)
     parser.add_argument("out_dir", help="Output directory")
     parser.add_argument("-it", "--iterations", help="Number of iterations to clean image. Default is 10000", type=int)
-    parser.add_argument("-st", "--steps", help="Number of intermediate steps after", type=int)
-    parser.add_argument("--seed", help="Random seed")
-    args = parser.parse_args()
+    parser.add_argument("-st", "--steps", help="Number of intermediate steps which will be printed in to an image",
+                        type=int)
+    # parser.add_argument("--seed", help="Random seed")
+    return parser.parse_args()
+
+
+def add_iteration_to_corner(im, text):
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 20)
+    draw.text((0, 0), text, 0, font=font)
+
+    return im
+
+
+def create_timelapse_gif(out_name, intermediate_steps, shape):
+    #print([st for st in intermediate_steps])
+    im = array_to_image(create_dotted_array(intermediate_steps[0], shape))
+    im_arr = [add_iteration_to_corner(array_to_image(create_dotted_array(st, shape)),"Iteration: " + str(it)) for it, st in intermediate_steps.items()]
+    im.save(out_name, save_all=True, append_images=im_arr, loop=0, duration=200)
+
+
+def main():
+    args = define_arguments()
+    filename = args.input_file.split('/')[-1]
+    im_arr = image_to_array(load_image(args.input_file))
+    shape = im_arr.shape
+    nodes, intermediate_nodes = tractor_beam(im_arr, args.k, args.iterations, args.steps)
+    # Save picture
+    array_to_image(create_dotted_array(nodes, shape)).save(args.out_dir + str(args.k) + filename)
+    # Save tsp-file
+    write_tsp_file(args.out_dir + filename.split('.')[0] + str(args.k) + '.tsp', nodes)
+    # Show all the intermediate steps
+    if args.steps is not None:
+        create_timelapse_gif(args.out_dir + filename.split('.')[0] + '.gif', intermediate_nodes, shape)
 
 
 if __name__ == '__main__':
